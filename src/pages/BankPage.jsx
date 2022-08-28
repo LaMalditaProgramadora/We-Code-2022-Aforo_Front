717770ca6cb999b69ac0bc93231092fb00c923ce
Jimena Ruiz <jruiz@novatronic.com>
2022-08-28 08:54:06 -0500

    new endpoint

--- src/pages/BankPage.jsx
+++ src/pages/BankPage.jsx
@@ -6,54 +6,32 @@ const BankPage = () => {
   const [name, setName] = useState("");
   const [idBank, setIdBank] = useState("");
 
-  //Ticket
-  const [capacityNormalWindow, setCapacityNormalWindow] = useState(0);
-  const [capacityNormalPlatform, setCapacityNormalPlatform] = useState(0);
-  const [capacityPrefWindow, setCapacityPrefWindow] = useState(0);
-  const [capacityPrefPlatform, setCapacityPrefPlatform] = useState(0);
+  const [capacity, setCapacity] = useState({
+    capacityNormalWindow: 0,
+    capacityNormalPlatform: 0,
+    capacityPrefWindow: 0,
+    capacityPrefPlatform: 0,
+    capacitySensorInt: 0,
+    capacityInt: 0,
+    capacityAtm: 0,
+    capacityTranQueue: 0,
+    capacityAtmQueue: 0
 
-  //Camera 1
-  const [capacityInt, setCapacityInt] = useState(0);
+  });
 
-  //Camera 2
-  const [capacityAtm, setCapacityAtm] = useState(0);
-
-  //Camera 3
-  const [capacityTranQueue, setCapacityTranQueue] = useState(0);
-  const [capacityAtmQueue, setCapacityAtmQueue] = useState(0);
-
-  //Sensor
-  const [capacitySensorInt, setCapacitySensorInt] = useState(0);
 
   useEffect(() => {
-      if (idBank !== "") {
-        socket.on(`Send Capacity by Camera 1 ${idBank}`, (data) => {
-          if (capacityInt !== data.capacityInt) setCapacityInt(data.capacityInt);
-        });
-        socket.on(`Send Capacity by Camera 2 ${idBank}`, (data) => {
-          if (capacityAtm !== data.capacityAtm) setCapacityAtm(data.capacityAtm);
-        });
-        socket.on(`Send Capacity by Camera 3 ${idBank}`, (data) => {
-          if (capacityTranQueue !== data.capacityTranQueue) setCapacityTranQueue(data.capacityTranQueue);
-          if (capacityAtmQueue !== data.capacityAtmQueue) setCapacityAtmQueue(data.capacityAtmQueue);
-        });
-        socket.on(`Send Capacity by Ticket ${idBank}`, (data) => {
-          if (capacityNormalWindow !== data.capacityNormalWindow) setCapacityNormalWindow(data.capacityNormalWindow);
-          if (capacityNormalPlatform !== data.capacityNormalPlatform) setCapacityNormalPlatform(data.capacityNormalPlatform);
-          if (capacityPrefWindow !== data.capacityPrefWindow) setCapacityPrefWindow(data.capacityPrefWindow);
-          if (capacityPrefPlatform !== data.capacityPrefPlatform) setCapacityPrefPlatform(data.capacityPrefPlatform);
-        });
-        socket.on(`Send Capacity by Sensor ${idBank}`, (data) => {
-          if (capacitySensorInt !== data.capacitySensorInt) setCapacitySensorInt(data.capacitySensorInt);
-        });
-      }
-  }, );
+    if (idBank !== "") {
+      socket.on(`input ${idBank}`, (data) => {
+        setCapacity(data);
+      });
+    }
+  });
 
   const listByIdFromApi = async (idBank) => {
     if (idBank === "") return;
     listById(idBank).then(
       (data) => {
-        console.log(data);
         if (data && data.data) {
           setName(data.data.name);
         }
@@ -65,7 +43,8 @@ const BankPage = () => {
   }
 
   const handleChange = (event) => {
-    setIdBank(event.target.value);
+    const idBank = event.target.value;
+    setIdBank(idBank);
   }
 
   const handleSubmit = () => {
@@ -86,45 +65,44 @@ const BankPage = () => {
       <label>Ticket: </label>
       <br></br>
       <label>capacityNormalWindow: </label>
-      <label>{capacityNormalWindow}</label>
+      <label>{capacity.capacityNormalWindow}</label>
       <br></br>
       <label>capacityNormalPlatform: </label>
-      <label>{capacityNormalPlatform}</label>
+      <label>{capacity.capacityNormalPlatform}</label>
       <br></br>
       <label>capacityPrefWindow: </label>
-      <label>{capacityPrefWindow}</label>
+      <label>{capacity.capacityPrefWindow}</label>
       <br></br>
       <label>capacityPrefPlatform: </label>
-      <label>{capacityPrefPlatform}</label>
+      <label>{capacity.capacityPrefPlatform}</label>
       <br></br>
       <br></br>
       <label>Sensor: </label>
       <br></br>
       <label>capacitySensorInt: </label>
-      <label>{capacitySensorInt}</label>
+      <label>{capacity.capacitySensorInt}</label>
       <br></br>
       <br></br>
       <label>Camera 1: </label>
       <br></br>
       <label>capacityInt: </label>
-      <label>{capacityInt}</label>
+      <label>{capacity.capacityInt}</label>
       <br></br>
       <br></br>
       <label>Camera 2: </label>
       <br></br>
       <label>capacityAtm: </label>
-      <label>{capacityAtm}</label>
+      <label>{capacity.capacityAtm}</label>
       <br></br>
       <br></br>
       <label>Camera 3: </label>
       <br></br>
       <label>capacityTranQueue: </label>
-      <label>{capacityTranQueue}</label>
+      <label>{capacity.capacityTranQueue}</label>
       <br></br>
       <label>capacityAtmQueue: </label>
-      <label>{capacityAtmQueue}</label>
+      <label>{capacity.capacityAtmQueue}</label>
       <br></br>
-
     </div>
   );
 };
